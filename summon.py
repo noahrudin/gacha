a634357209bc549ad76aa18366d52553e2f96678
Noah <noahrudin@gmail.com>
2020-05-22 11:01:39 -0500

    updated for 7pn

--- summon.py
+++ summon.py
@@ -14,7 +14,7 @@ import requests
 from io import BytesIO
 import xlrd
 #=======
-DISCORD_TOKEN = 'NzA1ODY1MjYyODI1NzM0MjI0.Xq95Fg.kSbAWYc0TeXVWk4JiK8iTBBYeMg'
+DISCORD_TOKEN = 'NzA1ODY1MjYyODI1NzM0MjI0.XsLR_Q.hzGdzgitHBLdzHLTMD5QLQstXO0'
 DISCORD_GUILD = 705865053689086032
 client = discord.Client()
 loc = ('SummonPool.xlsx')
@@ -22,13 +22,17 @@ wb = xlrd.open_workbook(loc)
 bfSheet = wb.sheet_by_index(0)
 bbSheet = wb.sheet_by_index(1)
 nbSheet = wb.sheet_by_index(2)
+specialSheet = wb.sheet_by_index(3)
+
 NUM_BRONZES = 15
 NUM_SILVERS = 48
 NUM_NB_GOLDS = 72
 NUM_BF_GOLDS = 127
 NUM_BB_GOLDS = 120
+NUM_SPECIAL_GOLDS = 72
 NUM_BFS = 53
 NUM_BBS = 48
+NUM_SPECIAL = 1
 #Used for changing rates
 ODDS_BF_GOLD = 15 #default 15
 ODDS_BF_SILVER = 62 #default 62
@@ -39,6 +43,11 @@ ODDS_BB_SILVER = 62  #default 62
 ODDS_BB_BRONZE = 23 #default 23
 #4 featured rn
 ODDS_BB_FEATURED = 2 #1% random
+
+ODDS_SPECIAL_GOLD = 15 #default 15
+ODDS_SPECIAL_SILVER = 62 #default 62
+ODDS_SPECIAL_BRONZE = 23  #default 23
+ODDS_SPECIAL_FEATURED = 1
 
 ODDS_NB_GOLD = 15  #default 15
 ODDS_NB_SILVER = 62  #default 62
@@ -110,9 +119,23 @@ async def on_message(message):
                 waitMessage = await message.channel.send('Please wait for the current multi-summon to complete!')
                 await waitMessage.delete(delay = 3)
                 return
+
+        if message.content==('.multi special'):
+            if LOCK == "unlocked":
+                lock = "unlocked"
+                await toggleLock(lock)
+                lock = "locked"
+                introMessage = await message.channel.send('Special 7* multi-summon for {0.name}:'.format(message.author))
+                author = str(message.author)
+                await specialMulti(author, message)
+                await introMessage.delete()
+            else:
+                waitMessage = await message.channel.send('Please wait for the current multi-summon to complete!')
+                await waitMessage.delete(delay = 3)
+                return
             
         if message.content==('.help'):
-            await message.channel.send(content = "Try \n`.multi nb` for Normal Banner Summons,\n`.multi bf` for Blazing Festival Summons, and \n`.multi bb` for Blazing Bash Summons. \nOnly one multi at a time!")
+            await message.channel.send(content = "Try \n`.multi nb` for Normal Banner Summons,\n`.multi bf` for Blazing Festival Summons, \n`.multi bb` for Blazing Bash Summons, and \n `.multi special` for 7-star summon. \nOnly one multi at a time!")
 
 
 @client.event
@@ -253,7 +276,7 @@ async def bbMulti(author, message):
             star1 = 3
             url1 = 4
             colorVar = 0x0ffd500
-            randCol = random.randint(116,119)
+            randCol = random.randint(73,121)
         if randSeed < ODDS_BB_GOLD and randSeed > ODDS_BB_FEATURED:
             id1 = 0
             name1 = 1
@@ -311,6 +334,79 @@ async def bbMulti(author, message):
     lock = "unlocked"
     await toggleLock(lock)
     
+@client.event
+async def specialMulti(author, message):
+    i = 1
+    while i < 11:
+        #randSeed=random.randint(0,(ODDS_BB_GOLD + ODDS_BB_SILVER + ODDS_BB_BRONZE)-1)
+        randSeed=random.randint(0,(ODDS_SPECIAL_GOLD + ODDS_SPECIAL_SILVER + ODDS_SPECIAL_BRONZE + ODDS_SPECIAL_FEATURED)-1)
+        #15% gold, 62% silver, 23% bronze
+        if randSeed <= ODDS_SPECIAL_FEATURED:
+            print("Rolled featured!")
+            id1 = 0
+            name1 = 1
+            desc1 = 2
+            star1 = 3
+            url1 = 4
+            colorVar = 0x4ad8ff
+            randCol = 72 #7pn featured
+        if randSeed < ODDS_SPECIAL_GOLD and randSeed > ODDS_SPECIAL_FEATURED:
+            id1 = 0
+            name1 = 1
+            desc1 = 2
+            star1 = 3
+            url1 = 4
+            colorVar = 0x0ffd500
+            #want to select a random gold from column list
+            randCol = random.randint(0,NUM_SPECIAL_GOLDS-1)
+            #guaranteed BB unit on gold
+            #randCol = random.randint(NUM_BB_GOLDS-NUM_BBS,NUM_BB_GOLDS-1)
+        elif randSeed >=ODDS_SPECIAL_GOLD and randSeed < (ODDS_SPECIAL_SILVER + ODDS_SPECIAL_GOLD):
+            id1 = 6
+            name1 = 7
+            desc1 = 8
+            star1 = 9
+            url1 = 10
+            colorVar = 0xd4d4d4
+            randCol = random.randint(0,NUM_SILVERS-1)
+        elif randSeed >= (ODDS_SPECIAL_SILVER + ODDS_SPECIAL_GOLD):
+            id1 = 12
+            name1 = 13
+            desc1 = 14
+            star1 = 15
+            url1 = 16
+            colorVar = 0xad7550
+            randCol = random.randint(0,NUM_BRONZES-1)
+        id = str(int(specialSheet.cell_value(randCol, id1)))
+        name = specialSheet.cell_value(randCol, name1)
+        desc = specialSheet.cell_value(randCol, desc1)
+        star = str(int(specialSheet.cell_value(randCol, star1)))
+        stitchedStarEmoji =  ':star: ' + star
+        url = specialSheet.cell_value(randCol, url1)
+        summonCount = "Summon " + str(i) + "/10"
+        embed = discord.Embed(title=name, description=desc,color=colorVar)
+        embed.set_thumbnail(url=str(url))
+        embed.add_field(name="ID", value = id, inline=True)
+        embed.add_field(name=summonCount, value = author, inline=True)
+        appendStr = str(i) + ' - '  +' **' + name + '**- ' + desc + " (" + stitchedStarEmoji + ") "
+        MULTI_LIST[i-1] = appendStr
+        if i == 1:
+            embedMessage = await message.channel.send(embed=embed)
+            await asyncio.sleep(1)
+        else:
+            await embedMessage.edit(embed=embed)
+            await asyncio.sleep(1)
+        i += 1
+    await embedMessage.delete()
+    appendedStr = ''
+    for x in MULTI_LIST:
+        appendedStr += x
+        appendedStr += '\n'
+    summaryEmbed = discord.Embed(title='Special banner featured results for ' + author + ": ", description = appendedStr, color=0xb52700) 
+    await message.channel.send(embed=summaryEmbed)
+    lock = "unlocked"
+    await toggleLock(lock)
+
 @client.event
 async def on_ready():
     print('Logged in as')
